bbcdbc71df9074ff1a0c9570206dac1b1ee15ac9
Kenny <kenny.rakotoarimanana@gmail.com>
2021-03-30 22:31:34 +0300

    Add pagination to matiere

--- controllers/matieres.controller.js
+++ controllers/matieres.controller.js
@@ -1,7 +1,7 @@
 let Matiere = require("../model/matiere");
 
 function getMatieres(req, res) {
-    Matiere.aggregate([
+    var aggregateQuery = Matiere.aggregate([
         {
             "$lookup": {
                 "from": "prof",
@@ -10,12 +10,25 @@ function getMatieres(req, res) {
                 "as": "prof"
             }
         },
-    ])
-    .then(matieres => {
-        res.send(matieres);
-    }).catch(err => {
-        res.send(err);
-    });
+    ]);
+    if (req.query.page) {
+        Matiere.aggregatePaginate(
+            aggregateQuery, {
+            page: parseInt(req.query.page) || 1,
+            limit: parseInt(req.query.limit) || 10,
+        })
+            .then(matieres => {
+                res.send(matieres);
+            }).catch(err => {
+                res.send(err);
+            });
+    } else {
+        aggregateQuery.then(matieres => {
+            res.send(matieres);
+        }).catch(err => {
+            res.send(err);
+        });
+    }
 }
 
 module.exports = {
